53c3766a8a5976ece9bd1a82ecc41e6a847d52f9
matiasfad7-spec <matiasfad7@gmail.com>
2026-06-23 16:39:40 -0500

    Add new attributes to EdgeDevice class

--- edgeDevice.py
+++ edgeDevice.py
@@ -2,4 +2,6 @@ from dataclasses import dataclass
 @dataclass
 class EdgeDevice:
     device_id: str
-    
+    draft_token_time: float
+    accuracypredcttion: float
+    numberOftokensGenerated: int

010bb45aabd052f333dd0602337dce6c40e19b9a
Sv-git1 <167702324+Sv-git1@users.noreply.github.com>
2026-07-01 14:05:45 -0700

    Plot.py, trying to add multiple lines

--- plot.py
+++ plot.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_results(xpoints, ypoints, xlabel, ylabel):
-    
-    plt.plot(xpoints, ypoints)
+
+def plot_results(xpoints, y_list, xlabel, ylabel, colors, line_style=''):
+    for i in range(len(y_list)):
+        plt.plot(xpoints, y_list[i], color=colors[i], linestyle=line_style, marker='o')
+
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
+    plt.legend()
     plt.show()

72d810b1a7d03f66bfe52ecce5ed484efbe87afc
ShrithaPabbichetty <shritha.pabbichetty@gmail.com>
2026-07-01 14:55:34 -0700

    Add error bars to plot_results function

--- plot.py
+++ plot.py
@@ -1,21 +1,26 @@
+
+
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_results(xpoints, y_list, xlabel, ylabel, colors, labels, line_style='-', marker='o'):
+def plot_results(xpoints, y_list, xlabel, ylabel, colors, labels, line_style='-', marker='o', yerr_list=None):
     if xpoints and isinstance(xpoints[0], (list, tuple, np.ndarray)):
         x_series = xpoints
     else:
         x_series = [xpoints] * len(y_list)
 
     for i in range(len(y_list)):
-        plt.plot(
+        plt.errorbar(
             x_series[0],
             y_list[i],
+            yerr=yerr_list[i] if yerr_list is not None else None,
             color=colors[i],
             linestyle=line_style[i] if isinstance(line_style, list) else line_style,
             marker=marker[i] if isinstance(marker, list) else marker,
-            label=labels[i]
+            label=labels[i],
+            capsize=4
         )
 
     plt.xlabel(xlabel)
